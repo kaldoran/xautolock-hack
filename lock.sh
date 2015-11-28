d804f4663926b7eb8c6a198cb2c8058d5d006e21
Kaldoran <kaldoran@live.fr>
2015-11-28 18:23:39 -0500

    Correction of the script, and remove notification off xautolock to add it in script

--- lock.sh
+++ lock.sh
@@ -1,33 +1,29 @@
 #/bin/bash
 
-# xautolock problem corrector by Reynaud Nicolas
-# https://github.com/kaldoran
-
-
 readonly CORNERSIZE=10 # Use same value use in xautolock config (default 10)
-
+readonly TIMER=10
 # If One of those app run, then do no run slock
 isAppRunning() {
     flash_process=`pgrep -l plugin-containe | grep -wc plugin-containe`
     if [[ $flash_process -ge 1 ]];then
-        return 1
+        return 0
     fi
     
     parole_process=`pgrep -lc parole`
     if [ $parole_process -ge 1 ]; then
-        return 1
+        return 0
     fi    
     
     impress_run=`ps -aux | grep "soffice.bin --impress" | wc -l`
     if [ $impress_run -ge 2 ]; then
-        return 1
+        return 0
     fi
 
-    return 0
+    return 1
 }
 
 # Hack the mouse in corner
-inCorner() {
+isInCorner() {
 
     # Screen height (SCREEN_Y) and Width (SCREEN_X)
     eval $(xrandr | grep '*' | grep -o -E "[0-9]+x[0-9]+" | sed -e "s/\([0-9]*\)/SCREEN_X=\1/" -e "s/x\([0-9]*\)/\nSCREEN_Y=\1/")
@@ -52,16 +48,18 @@ inCorner() {
 
 }
 
-if inCorner; then
+if isInCorner; then
     slock;
 fi
 
 if isAppRunning; then
     dpmsStatus=`xset -q | grep -ce 'DPMS is Enabled'` # DPMS is at 1 if you check "Presentation mod"
     if [ $dpmsStatus == 1 ];then
+        notify-send -t 5000 -- "Lock screen in $TIMER seconds"
+        sleep $TIMER;  
         slock;
     fi
 fi
 
 # Here is my command :D
-# xautolock -detectsleep -time 1 -locker "~/lock.sh" -nowlocker "slock" -notify 30 -notifier "notify-send -t 5000 -- 'Lock screen in 30 seconds'" -corners 000+ -cornerdelay 3
+# xautolock -detectsleep -time 1 -locker "~/lock.sh" -nowlocker "slock" -corners 000+ -cornerdelay 3
